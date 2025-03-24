7b8757ae971e6c3e9a684dcd46cbeb9591f1493a
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-03-24 08:30:56 +0100

    first try on code, not tested yet

--- check_if_palindrome/Program.cs
+++ check_if_palindrome/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("please enter a word to check if it's a palindrome:");
+        string userInput = Console.ReadLine();
+        int i = 0;
+        int j = userInput.Length - 1;
+
+        for (i = 0; i < j;)
+        {
+            if (userInput[i] == j)
+            {
+                i++;
+                j -= 1;
+            }
+            else
+            {
+                Console.WriteLine("your entered word isn't a palindrome.");
+            }
+        }
+        Console.WriteLine("your word is a palindrome!");
+    }
+}
